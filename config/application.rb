8c2f5d2b6e03f8f5ee7ac6ff076b122e26b66f0a
Matoba Taiki <tomatomatobanana@gmail.com>
2020-07-21 15:30:46 +0900

    rails generateの自動生成ファイルを設定

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Webapp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.generators do |g|
+      g.assets false          # CSS, JavaScriptファイルは生成しない
+      g.test_framework false  # testファイルは生成しない
+    end
+  end
+end
