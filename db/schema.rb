c2ad44ccbabee06540c484f1ffbcb1fb185af6b1
Matoba Taiki <tomatomatobanana@gmail.com>
2020-07-23 14:26:28 +0900

    add: add password column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_082952) do
+ActiveRecord::Schema.define(version: 2020_07_23_052233) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_07_22_082952) do
     t.string "check_in"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
