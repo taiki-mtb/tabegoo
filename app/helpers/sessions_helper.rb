56daeda4b69c5b6592cd32826c4bb4a7d8684060
Matoba Taiki <tomatomatobanana@gmail.com>
2020-07-28 17:47:34 +0900

    fix: rubocopが通る様に修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,8 +10,6 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
@@ -38,5 +36,4 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-
 end
