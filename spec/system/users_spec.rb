504cdf01ee73bfd44544ac043b0ac3737437dc8f
Matoba Taiki <tomatomatobanana@gmail.com>
2020-07-29 16:02:38 +0900

    test: ログインしているかどうかでの挙動の違いをテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -42,6 +42,7 @@ RSpec.describe "Users", type: :system do
 
   describe "プロフィール編集ページ" do
     before do
+      login_for_system(user)
       visit user_path(user)
       click_link "プロフィール編集"
     end
@@ -77,6 +78,7 @@ RSpec.describe "Users", type: :system do
   describe "プロフィールページ" do
     context "ページレイアウト" do
       before do
+        login_for_system(user)
         visit user_path(user)
       end
 
