1baaf1bb1827b46f1300c20ac492fdf7a0f4fb8e
Matoba Taiki <tomatomatobanana@gmail.com>
2020-08-05 01:41:23 +0900

    add: レストラン登録ページの実装

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,11 @@
+class RestaurantsController < ApplicationController
+
+  def new
+    if current_user.admin?
+      @restaurant = Restaurant.new
+    else
+      flash[:danger] = "管理人しか作成できません"
+      redirect_to root_url
+    end
+  end
+end
