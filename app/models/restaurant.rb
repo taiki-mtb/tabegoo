6dda6f87482842873f6b7b57e875709527d6d0d7
Matoba Taiki <tomatomatobanana@gmail.com>
2020-08-05 18:01:37 +0900

    レストラン登録ページへのリンク追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,4 @@
 class Restaurant < ApplicationRecord
-  belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 30 }
