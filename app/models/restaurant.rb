99bf33273604be7a0b57f251260638bbd9523f62
Matoba Taiki <tomatomatobanana@gmail.com>
2020-08-08 22:37:08 +0900

    add: 写真投稿機能の実装

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,13 @@ class Restaurant < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 140 }
+  validate  :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "：5MBより大きい画像はアップロードできません。")
+      end
+    end
 end
