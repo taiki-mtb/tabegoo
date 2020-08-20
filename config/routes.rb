d205b54da8decb203dead8db41f00dee4ebf3abf
Matoba Taiki <tomatomatobanana@gmail.com>
2020-08-20 15:18:13 +0900

    add: notificationコントローラーとそのテストの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'notifications/index'
  root 'static_pages#home'
   get :signup,       to: 'users#new'
   resources :users
   resources :restaurants do
-    resource :favorites, only: [:create, :destroy ]
+    resource :favorites,    only: [:create, :destroy ]
   end
-  resources :comments, only: [:create, :destroy]
+  resources :comments,      only: [:create, :destroy]
+  resources :notifications, only: :index
   get    :login,         to: 'sessions#new'
   post   :login,         to: 'sessions#create'
   delete :logout,        to: 'sessions#destroy'
