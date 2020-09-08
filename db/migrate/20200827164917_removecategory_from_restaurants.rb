79bf1d4956b4bab57d69794808cad0a3d4e2174f
Matoba Taiki <tomatomatobanana@gmail.com>
2020-09-08 19:45:29 +0900

    fix:不要なmigrateファイルの削除

--- db/migrate/20200827164917_removecategory_from_restaurants.rb
+++
@@ -1,9 +0,0 @@
-class RemovecategoryFromRestaurants < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :restaurants, :category
-  end
-
-  def down
-    add_column :restaurants, :category
-  end
-end
