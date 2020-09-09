1e6f17db96da55d8ee23fc497ceef77f98b395a4
Matoba Taiki <tomatomatobanana@gmail.com>
2020-09-09 17:22:37 +0900

    puma.rbの書き換え

--- config/puma.rb
+++ config/puma.rb
@@ -7,4 +7,9 @@ plugin :tmp_restart
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+rails_env = ENV.fetch("RAILS_ENV") { "production" }
+if rails_env == "production"
+　daemonize true
+　stdout_redirect "/var/log/puma/puma.stdout.log", "/var/log/puma/puma.stderr.log", true
+end
+environment rails_env
