3ce095cc64d11bf1ca2409a2e6971d193cb5254d
Matoba Taiki <tomatomatobanana@gmail.com>
2020-09-10 14:14:37 +0900

    config/puma.rbの変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,19 @@
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
+plugin :tmp_restart
+
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+rails_env = ENV.fetch("RAILS_ENV") { "production" }
+if rails_env == "production"
+  daemonize true
+  stdout_redirect "/var/log/puma/puma.stdout.log", "/var/log/puma/puma.stderr.log", true
+end
+environment rails_env
+
 workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
