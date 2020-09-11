253a6c8402ecc6cf70ffb48ebd639f9e8e5566f6
Matoba Taiki <tomatomatobanana@gmail.com>
2020-09-11 17:13:08 +0900

    seedの内容を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,11 @@ User.create!(name:  "マネージャー　まとば",
               password:              password,
               password_confirmation: password)
 end
+
+Category.create!(name: "居酒屋")
+
+Restaurant.create!(name: "レストランexample",
+               description: "冬に行きたい、身体が温まるレストランです",
+               category_id: 1,
+               association :category)
+end
