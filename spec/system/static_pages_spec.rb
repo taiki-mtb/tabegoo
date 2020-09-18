8c6250986263e8e82054f8991a4a89c648588506
Matoba Taiki <tomatomatobanana@gmail.com>
2020-09-18 17:17:52 +0900

    お気に入りページの修正

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -10,25 +10,21 @@ RSpec.describe "StaticPages", type: :system do
       visit root_path
     end
 
-    it "ポートフォリオの文字列が存在すること" do
-      expect(page).to have_content 'ポートフォリオ'
-    end
-
     it "正しいタイトルが表示されること" do
       expect(page).to have_title full_title
     end
 
     context "管理者ユーザーの場合" do
-      it "「新しいレストランの登録」リンクが表示されること" do
+      it "「新しいお店の登録」リンクが表示されること" do
         login_for_system(admin_user)
         expect(page).to have_link "新しいレストランの登録", href: new_restaurant_path
       end
     end
 
     context "管理者以外のユーザーの場合" do
-      it "「新しいレストランの登録」リンクが表示されないこと" do
+      it "「新しいお店の登録」リンクが表示されないこと" do
         login_for_system(user)
-        expect(page).not_to have_link "新しいレストランの登録"
+        expect(page).not_to have_link "新しいお店の登録"
       end
     end
 
